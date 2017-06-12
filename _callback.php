41b87a4af2a394622c4a658e914b95cb8aaf55df
iglazer <ian.glazer@gmail.com>
2017-06-12 18:14:14 -0400

    updated _callback

--- _callback.php
+++ _callback.php
@@ -5,7 +5,7 @@
 	<meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
 	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_ALLOWED_DOMAINS');?>">
-	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?><?php echo getenv('SALESFORCE_JAVASCRIPT_SERVLET');?>" async defer></script>
+	<script src="<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 </head> 
 <body></body>    
 </html>
