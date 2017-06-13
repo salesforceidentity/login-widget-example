ea3bc734abfb3462bd68ae7806e37a086387899c
iglazer <ian.glazer@gmail.com>
2017-06-13 11:54:16 -0400

    Tweaked callback

--- _callback.php
+++ _callback.php
@@ -4,7 +4,7 @@
     <meta name="salesforce-community" content="<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
 	<meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>-callback">
 	<meta name="salesforce-save-access-token" content="true">
-	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_ALLOWED_DOMAINS');?>">
+	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>">
 	<script src="<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
 </head> 
 <body></body>    
